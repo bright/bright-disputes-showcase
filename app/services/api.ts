712bea749d3c868feb399a2163c51353c6279bd5
Krystian Kaminski <krystian.kaminski@brightinventions.pl>
2024-01-08 16:45:29 +0100

    Reuse API instance

--- app/services/api.ts
+++ app/services/api.ts
@@ -7,22 +7,14 @@ import type { WeightV2 } from "@polkadot/types/interfaces";
 const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
 const PROOF_SIZE = new BN(1_000_000);
 
-const getAPI = async () => {
-  const connect = async () => {
-    const wsProvider = new WsProvider(NODE_ADDRESS);
-    const api = new ApiPromise({provider: wsProvider});
-
-    return api.isReady;
-  };
-
-  return await connect();
-}
+const wsProvider = new WsProvider(NODE_ADDRESS);
+const promise = new ApiPromise({provider: wsProvider});
+const API = await promise.isReady;
 
 export const query = async <T, >(callerKey: string, method: string, ...params: unknown[]) => {
-  const api = await getAPI();
-  const contract = new ContractPromise(api, abi, CONTRACT_ADDRESS);
+  const contract = new ContractPromise(API, abi, CONTRACT_ADDRESS);
   const {result, output} = await contract.query[method](callerKey, {
-    gasLimit: api?.registry.createType('WeightV2', {
+    gasLimit: API?.registry.createType('WeightV2', {
       refTime: MAX_CALL_WEIGHT,
       proofSize: PROOF_SIZE,
     }) as WeightV2,
@@ -36,15 +28,14 @@ export const query = async <T, >(callerKey: string, method: string, ...params: u
 }
 
 export const transaction = async (callerSeed: string, method: string, ...params: unknown[]) => {
-  const api = await getAPI();
   const keyring = new Keyring({type: 'sr25519'});
   const accountKeyring = keyring.addFromUri(callerSeed);
-  const contract = new ContractPromise(api, abi, CONTRACT_ADDRESS);
+  const contract = new ContractPromise(API, abi, CONTRACT_ADDRESS);
 
   return new Promise((resolve) => {
     contract.tx[method]({
         value: 0,
-        gasLimit: api?.registry.createType('WeightV2', {
+        gasLimit: API?.registry.createType('WeightV2', {
           refTime: new BN(5_000_000_000).isub(BN_ONE),
           proofSize: PROOF_SIZE,
         }) as WeightV2,
