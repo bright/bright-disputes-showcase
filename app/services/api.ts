284334bee5a5b7b3525c6fcc56187c9001d98fa3
Krystian Kaminski <krystian.kaminski@brightinventions.pl>
2024-01-12 14:10:13 +0100

    Gas limit fix

--- app/services/api.ts
+++ app/services/api.ts
@@ -4,9 +4,8 @@ import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import { ContractPromise } from "@polkadot/api-contract";
 import type { WeightV2 } from "@polkadot/types/interfaces";
 
-const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
-const PROOF_SIZE = new BN(1_000_000);
-
+const refTime = new BN(100_000_000_000);
+const proofSize = new BN(100_000_000_000);
 const wsProvider = new WsProvider(NODE_ADDRESS);
 const promise = new ApiPromise({provider: wsProvider});
 const API = await promise.isReady;
@@ -14,10 +13,7 @@ const API = await promise.isReady;
 export const query = async <T, >(callerKey: string, method: string, ...params: unknown[]) => {
   const contract = new ContractPromise(API, abi, CONTRACT_ADDRESS);
   const {result, output} = await contract.query[method](callerKey, {
-    gasLimit: API?.registry.createType('WeightV2', {
-      refTime: MAX_CALL_WEIGHT,
-      proofSize: PROOF_SIZE,
-    }) as WeightV2,
+    gasLimit: API?.registry.createType('WeightV2', { refTime, proofSize }) as WeightV2,
   }, ...params);
 
   if (!result.isOk) {
@@ -35,14 +31,20 @@ export const transaction = async (callerSeed: string, method: string, ...params:
   return new Promise((resolve) => {
     contract.tx[method]({
         value: 0,
-        gasLimit: API?.registry.createType('WeightV2', {
-          refTime: new BN(5_000_000_000).isub(BN_ONE),
-          proofSize: PROOF_SIZE,
-        }) as WeightV2,
+        gasLimit: API?.registry.createType('WeightV2', { refTime, proofSize }) as WeightV2,
       }, ...params)
-      .signAndSend(accountKeyring, ({ status }) => {
-        if (status.type === 'Finalized') {
-          resolve(true);
+      .signAndSend(accountKeyring, ({ status, events  }) => {
+        if (status.isInBlock || status.isFinalized) {
+          const failedEvents = events
+            .filter(({ event }) =>
+              API.events.system.ExtrinsicFailed.is(event)
+            );
+
+          failedEvents.forEach(({ event: { data: [error, info] } }) => {
+              console.log(error.toString());
+            });
+
+          resolve(!failedEvents.length);
         }
       });
     }
